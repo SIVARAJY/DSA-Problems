24664712e9da881c33edc86b96fc9db092991362
SIVARAJ Y <sivaraj262300@gmail.com>
2026-09-11 14:55:30 +0530

    Time: 1 ms (17.12%) | Memory: 42.1 MB (72.02%) - LeetSync

---
+++ Leetcode/476-number-complement/number-complement.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findComplement(int num) {
+        StringBuilder s = new StringBuilder(Integer.toBinaryString(num));
+        int n =s.length();
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='1') s.setCharAt(i,'0');
+            else if(s.charAt(i)=='0') s.setCharAt(i,'1');
+        }
+        return Integer.parseInt(s.toString(),2);
+        
+    }
+}
