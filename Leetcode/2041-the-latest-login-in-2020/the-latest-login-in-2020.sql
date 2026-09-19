b9b476aa03613b0df33886958de1affa36ef2d85
SIVARAJ Y <sivaraj262300@gmail.com>
2026-09-19 15:04:48 +0530

    Time: 699 ms (58.62%) | Memory: 0B (100.00%) - LeetSync

--- Leetcode/2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ Leetcode/2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -1,5 +1,2 @@
-select user_id, max(time_stamp) as last_stamp
-from logins
-where time_stamp>='2020-01-01'
-and time_stamp<'2021-01-01'
-group by user_id;
+# Write your MySQL query statement below
+select user_id, max(time_stamp) as last_stamp from Logins where year(time_stamp)='2020' group by user_id ;
