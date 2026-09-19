06b05295d0e9a7dd5f163809ad14d3a0b7b9f0d6
SIVARAJ Y <sivaraj262300@gmail.com>
2026-09-19 06:46:06 +0530

    Time: 369 ms (93.14%) | Memory: 0B (100.00%) - LeetSync

--- Leetcode/181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ Leetcode/181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,4 +1,5 @@
-select e1.name as Employee 
-from Employee e1 
-where e1.salary>
-(select salary from Employee e2 where e1.managerId=e2.id); 
+select e1.name as employee
+from employee as e1
+join employee as e2
+on e1.managerid=e2.id
+where e1.salary>e2.salary;
