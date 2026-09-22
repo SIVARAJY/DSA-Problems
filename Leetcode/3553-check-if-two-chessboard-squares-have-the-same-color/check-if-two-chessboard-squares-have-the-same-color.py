478a89f72ddb387a3e4ee42d122d894363dc17da
SIVARAJ Y <sivaraj262300@gmail.com>
2026-09-22 14:55:34 +0530

    Time: 37 ms (90.63%) | Memory: 19.3 MB (17.08%) - LeetSync

--- Leetcode/3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.py
+++ Leetcode/3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.py
@@ -1,8 +1,13 @@
 class Solution:
-    def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
-        x1, y1 = coordinate1[0], coordinate1[1]
-        x2, y2 = coordinate2[0], coordinate2[1]
-        x_diff = abs(ord(x1) - ord(x2))
-        y_diff = abs(ord(y1) - ord(y2))
+    def squareIsWhite(self, coordinates: str) -> bool:
+        col,row=list(coordinates)[0],list(coordinates)[1]
+        if(col=="a" or col=="c" or col=="e" or col=="g") and int(row)%2==1:
+            return False
+        if(col=="b" or col=="d" or col=="f" or col=="h") and int(row)%2==0:
+            return False
+        return True    
 
-        return (x_diff & 1) == (y_diff & 1)
+    def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
+        if (self.squareIsWhite(coordinate1) and self.squareIsWhite(coordinate2)) or (not self.squareIsWhite(coordinate1) and not self.squareIsWhite(coordinate2)):
+            return True
+        return False    
