7503611b29206c3eb8165b8266fd806722339076
SIVARAJ Y <sivaraj262300@gmail.com>
2026-09-24 11:00:46 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (26.61%) - LeetSync

---
+++ Leetcode/179-largest-number/largest-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestNumber(self, nums: list[int]) -> str:
+        array=list(map(str,nums))
+
+        array.sort(key=lambda x:x*10,reverse=True)
+        
+        if array[0]=="0":
+            return "0"
+        length="".join(array)
+        return length
