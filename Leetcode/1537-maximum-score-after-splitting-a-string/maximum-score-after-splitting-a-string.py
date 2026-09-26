a2fd30157cacdd1dcbb28f4b4341dc735c041392
SIVARAJ Y <sivaraj262300@gmail.com>
2026-09-26 20:43:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (18.32%) - LeetSync

---
+++ Leetcode/1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        ones_right = s.count('1')
+        zeros_left = 0
+        max_score = 0
+        
+        for i in range(len(s) - 1):
+            if s[i] == '0':
+                zeros_left += 1
+            else:
+                ones_right -= 1
+                
+            max_score = max(max_score, zeros_left + ones_right)
+            
+        return max_score
